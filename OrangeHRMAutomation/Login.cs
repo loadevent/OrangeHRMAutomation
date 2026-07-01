567ef8d0f4f2ae61fae0abe5649bbda8d4f2ca62
loadevent <loadevent.academy@gmail.com>
2026-07-01 16:35:54 +0200

    Add project files.

---
+++ OrangeHRMAutomation/Login.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OrangeHRMAutomation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ValidLogi()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
+
+            driver.FindElement(By.Name("username")).SendKeys("Admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin123");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+
+
+            Assert.Pass();
+        }
+    }
+}
