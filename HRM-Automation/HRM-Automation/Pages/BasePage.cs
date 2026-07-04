170bc722f1ab5899371fc535343805d70eb20907
loadevent <loadevent.academy@gmail.com>
2026-07-05 01:04:53 +0200

    Page objects created (Admin, Dashboard, Leave, Login, Base)

---
+++ HRM-Automation/HRM-Automation/Pages/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM_Automation.Pages
+{
+    internal class BasePage
+    {
+        protected IWebDriver driver { get; set; }
+
+        public BasePage(WebDriver myDriver)
+        {
+            this.driver = myDriver;
+        }
+    }
+}
---
+++ HRM-Automation/HRM-Automation/Pages/DashboardPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM_Automation.Pages
+{
+    internal class DashboardPage
+    {
+    }
+}
